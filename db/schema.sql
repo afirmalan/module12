0ad032bc0b9c93537fceb3d80642686b616a1968
Adrian Firmalan <afirmalan@yahoo.com>
2022-09-05 10:52:31 -0700

    fixed bug on employee table

--- db/schema.sql
+++ db/schema.sql
@@ -9,6 +9,7 @@ CREATE TABLE department(
     name VARCHAR (30) NOT NULL
 );
 
+
 CREATE TABLE role(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR (30),
@@ -17,12 +18,12 @@ CREATE TABLE role(
     FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE CASCADE
 );
 
-CREATE TABLE employee (
+CREATE TABLE employee(
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR (30),
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE FOREIGN KEY (manager_id) REFERENCES employee (id) ON DELETE
-    SET NULL
+    FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE, 
+    FOREIGN KEY (manager_id) REFERENCES employee (id) ON DELETE SET NULL
 );
