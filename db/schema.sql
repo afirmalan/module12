bc3280c2c80a7adda156d73587c839ec877fc930
Adrian Firmalan <afirmalan@yahoo.com>
2022-09-05 10:33:01 -0700

    formatting

--- db/schema.sql
+++ db/schema.sql
@@ -10,20 +10,19 @@ CREATE TABLE department(
 );
 
 CREATE TABLE role(
-    id  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR (30),
-    salary DECIMAL(10,2),
+    salary DECIMAL(10, 2),
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department (id)
-    ON DELETE CASCADE
+    FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE CASCADE
 );
 
-CREATE TABLE employee(
+CREATE TABLE employee (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR (30),
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE
-    FOREIGN KEY (manager_id) REFERENCES employee (id) ON DELETE SET NULL
+    FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE CASCADE FOREIGN KEY (manager_id) REFERENCES employee (id) ON DELETE
+    SET NULL
 );
