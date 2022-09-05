c70298d03412963c1e7e671af7eb158821b21d15
Adrian Firmalan <afirmalan@yahoo.com>
2022-09-05 10:21:19 -0700

    added seeds and queries sql from class

---
+++ db/queries.sql
@@ -0,0 +1,11 @@
+USE employee_db;
+
+
+SELECT employee.id, employee.first_name, employee.last_name, 
+role.title, department.name, role.salary, 
+CONCAT(mgr.first_name," ", mgr.last_name) as manager
+
+FROM employee
+LEFT JOIN role ON role.id= employee.role_id
+LEFT JOIN department ON role.department_id=department_id
+LEFT JOIN employee as mgr ON employee.id- mgr.manager_id
